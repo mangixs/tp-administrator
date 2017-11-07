6559503e9d9ce45939a59f91995f154a3629c7fa
mangixs <fnhdcwy@hotmail.com>
2017-11-07 17:52:28 +0800

    添加文章管理和幻灯片管理

--- application/admin/controller/Slide.php
+++ application/admin/controller/Slide.php
@@ -7,29 +7,28 @@ use BaseAdmin;
 use FormCheck;
 use AdminModel;
 
-class Article extends BaseAdmin{
+class Slide extends BaseAdmin{
 	private $rule=[
-		'article_form'=>[
+		'slide_form'=>[
 			'title'=>['name'=>'title','preg'=>':notnull','notice'=>'请输入标题!'],
-            'type'=>['name'=>'type','preg'=>':number','notice'=>'请选择文章类型!'],
-            'content'=>['name'=>'content','preg'=>':notnull','notice'=>'请输入详情!'],
-            'show_place'=>['name'=>'show_place','preg'=>'/^[1|2]{1}$/','notice'=>'请选择展示位置'],
-            'first_img'=>['name'=>'first_img','preg'=>':notnull','notice'=>'请上传新闻首图'],
-            'is_show'=>['name'=>'is_show','preg'=>'/^[1|2]{1}$/','notice'=>'请选择是否展示'],				
+            'type'=>['name'=>'type','preg'=>':number','notice'=>'请选择幻灯片类型!'],
+            'url'=>['name'=>'url','preg'=>':url','notice'=>'请输入正确的链接!','not_null'=>false],
+            'is_show'=>['name'=>'is_show','preg'=>'/^[1|2]{1}$/','notice'=>'请选择展示位置'],
+            'img'=>['name'=>'img','preg'=>':notnull','notice'=>'请上传图片'],				
 		],
 	];
-	private $articleType=['1'=>'新闻','2'=>'公司文化'];
+	private $slideType=['1'=>'MOBILE','2'=>'PC'];
     public function index(){
 		$sessionkey=session('staff_session_object');
-        $this->assign('key',$sessionkey['key']['article']);
+        $this->assign('key',$sessionkey['key']['slide']);
         return view(); 	
     }
     public function pageData(){
 		$m = new AdminModel();
-    	$db=db('article');
+    	$db=db('slide');
     	$ret['page']=$m->setPage($db);
     	$m->setSearch($db);
-    	$ret['data']=$db->field('id,title,type,update_at,first_img,sort')->select();
+    	$ret['data']=$db->field('id,title,type,update_at,img,sort')->select();
     	foreach ($ret['data'] as $key => &$v) {
     		$v['update_at']=date('Y-m-d H:i:s',$v['update_at']);
     	}
@@ -37,7 +36,7 @@ class Article extends BaseAdmin{
     }
     public function add(){
     	$this->assign('action','add');
-    	$this->assign('articleType',$this->articleType);
+    	$this->assign('slideType',$this->slideType);
     	return view();
     }
     public function edit($id,$act){
@@ -45,9 +44,9 @@ class Article extends BaseAdmin{
     		return json(['result'=>'ERROR','msg'=>'参数错误']);
     	}
     	$this->assign('action',$act);
-    	$data=db('article')->where('id',$id)->find();
+    	$data=db('slide')->where('id',$id)->find();
     	$this->assign('data',$data);
-    	$this->assign('articleType',$this->articleType);
+    	$this->assign('slideType',$this->slideType);
     	return $this->fetch('add');    	
     }
     public function save(){
@@ -55,12 +54,10 @@ class Article extends BaseAdmin{
 		$data['type']=input('post.type');
 		$data['is_show']=input('post.is_show');
 		$data['sort']=input('post.sort');
-		$data['show_place']=input('post.show_place');
-		$data['first_img']=input('post.first_img');
-		$data['summary']=input('post.summary');
-		$data['content']=$_POST['content'];
+		$data['img']=input('post.img');
+		$data['url']=input('post.url');
 		$formObj=new FormCheck($this->rule);
-		$checkResult=$formObj->checkFrom($data,'article_form');
+		$checkResult=$formObj->checkFrom($data,'slide_form');
 		if ( $checkResult['result'] !=='CHECK_PASS' ) {
 			return json($checkResult);
 		}
@@ -69,11 +66,11 @@ class Article extends BaseAdmin{
 		switch ($action) {
 			case 'add':
 				$data['insert_at']=$data['update_at'];
-				$id=db('article')->insertGetId($data);
+				$id=db('slide')->insertGetId($data);
 				break;
 			case 'edit':
 				$id=input('post.id');
-				db('article')->where('id',$id)->update($data);
+				db('slide')->where('id',$id)->update($data);
 				break;
 			default:
 				# code...
@@ -81,11 +78,11 @@ class Article extends BaseAdmin{
 		}
 		return json(['result'=>'SUCCESS','msg'=>'操作成功','id'=>$id]);    	
     }
-    public function deleteArticle($id){
+    public function deleteSlide($id){
     	if ( !is_numeric($id) ) {
     		return json(['result'=>'ERROR','msg'=>'参数错误']);
     	}
-    	db('article')->where('id',$id)->delete();
+    	db('slide')->where('id',$id)->delete();
     	return json(['result'=>'SUCCESS','msg'=>'删除成功']);
     }
 }
